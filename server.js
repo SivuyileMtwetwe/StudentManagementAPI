e143e790d360a2f5a63be893ea0e13f2d798ffa1
sivuyilemtwetwe <sivuyilemtwetwe@gmail.com>
2024-09-05 15:14:13 +0200

    Update

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+app.get('/',  async(req, res) => {
+    res.status(200).send("Welcome to the student management api")
+})
